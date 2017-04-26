3872d3374814e069af547cf1d74a0878a34c08fa
Miles Robson <miles_robson@yahoo.com>
2017-04-25 20:09:35 -0400

    Up to lesson 33, messing with styles and such.

--- routes/app.js
+++ routes/app.js
@@ -1,30 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({},function(err, doc){
-        if(err)
-        {
-            return res.send('Error, no user');
-        }
-        res.render('node', {email: doc.email});
+        res.render('index');
 
     });
-    //Moved, to now render if no error res.render('node');
-});
-
-router.post('/', function (req,res,next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Miles',
-        lastName: 'Robson',
-        password: 'test',
-        email: email
-    });
-
-    user.save();
-    res.redirect('/');
-});
 
 module.exports = router;
